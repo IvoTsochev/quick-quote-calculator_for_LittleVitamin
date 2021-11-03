0e2d4a282770eb5be998a24fe8a68168c3c6faf0
Ivaylo Tsochev <ivo.tsochev@live.com>
2021-11-03 11:18:01 +0200

    fixing bug with features styling when selected

--- src/components/Q7-2-AdditionalFunc/Q72AdditionalFunc.jsx
+++ src/components/Q7-2-AdditionalFunc/Q72AdditionalFunc.jsx
@@ -15,10 +15,14 @@ import { pageAnimation } from "../../animation";
 import { forwardAnime } from "../../util";
 
 const Q72AdditionalFunc = ({ storePrices, setStorePrices }) => {
+
+
+
+
   return (
     <motion.div
       className="q72-additional-func page"
-      variants={pageAnimation}
+      variants={ pageAnimation }
       initial="hidden"
       animate="show"
       exit="exit"
@@ -32,111 +36,111 @@ const Q72AdditionalFunc = ({ storePrices, setStorePrices }) => {
       </p>
       <div className="many-pages-add">
         <ul className="ks-cboxtags">
-          <li>
+          <li style={ storePrices['Photo gallery +£170'] ? { opacity: '50%' } : { opacity: '100%' } }>
             <SingleCheckBtn
-              price={prices.Q72_Photo_gallery}
-              name={"Q72 Photo gallery"}
-              label={"Photo gallery +£170"}
-              storePrices={storePrices}
-              setStorePrices={setStorePrices}
+              price={ prices.Q72_Photo_gallery }
+              name={ "Q72 Photo gallery" }
+              label={ "Photo gallery +£170" }
+              storePrices={ storePrices }
+              setStorePrices={ setStorePrices }
             />
           </li>
-          <li>
+          <li style={ storePrices['Newsletter sign-up +£170'] ? { opacity: '50%' } : { opacity: '100%' } }>
             <SingleCheckBtn
-              price={prices.Q72_Newsletter_sign_up}
-              name={"Q72 Newsletter sign-up"}
-              label={"Newsletter sign-up +£170"}
-              storePrices={storePrices}
-              setStorePrices={setStorePrices}
+              price={ prices.Q72_Newsletter_sign_up }
+              name={ "Q72 Newsletter sign-up" }
+              label={ "Newsletter sign-up +£170" }
+              storePrices={ storePrices }
+              setStorePrices={ setStorePrices }
             />
           </li>
-          <li>
+          <li style={ storePrices['Video content £255'] ? { opacity: '50%' } : { opacity: '100%' } }>
             <SingleCheckBtn
-              price={prices.Q72_Video_content}
-              name={"Q72 Video content"}
-              label={"Video content £255"}
-              storePrices={storePrices}
-              setStorePrices={setStorePrices}
+              price={ prices.Q72_Video_content }
+              name={ "Q72 Video content" }
+              label={ "Video content £255" }
+              storePrices={ storePrices }
+              setStorePrices={ setStorePrices }
             />
           </li>
-          <li>
+          <li style={ storePrices['Age gate +£255'] ? { opacity: '50%' } : { opacity: '100%' } }>
             <SingleCheckBtn
-              price={prices.Q72_Age_gate}
-              name={"Q72 Age gate"}
-              label={"Age gate +£255"}
-              storePrices={storePrices}
-              setStorePrices={setStorePrices}
+              price={ prices.Q72_Age_gate }
+              name={ "Q72 Age gate" }
+              label={ "Age gate +£255" }
+              storePrices={ storePrices }
+              setStorePrices={ setStorePrices }
             />
           </li>
-          <li>
+          <li style={ storePrices['File sharing +£340'] ? { opacity: '50%' } : { opacity: '100%' } }>
             <SingleCheckBtn
-              price={prices.Q72_File_sharing}
-              name={"Q72 File sharing"}
-              label={"File sharing +£340"}
-              storePrices={storePrices}
-              setStorePrices={setStorePrices}
+              price={ prices.Q72_File_sharing }
+              name={ "Q72 File sharing" }
+              label={ "File sharing +£340" }
+              storePrices={ storePrices }
+              setStorePrices={ setStorePrices }
             />
           </li>
-          <li>
+          <li style={ storePrices['Booking schedule +£510'] ? { opacity: '50%' } : { opacity: '100%' } }>
             <SingleCheckBtn
-              price={prices.Q72_Booking_schedule}
-              name={"Q72 Booking schedule"}
-              label={"Booking schedule +£510"}
-              storePrices={storePrices}
-              setStorePrices={setStorePrices}
+              price={ prices.Q72_Booking_schedule }
+              name={ "Q72 Booking schedule" }
+              label={ "Booking schedule +£510" }
+              storePrices={ storePrices }
+              setStorePrices={ setStorePrices }
             />
           </li>
-          <li>
+          <li style={ storePrices['Contact form +£170'] ? { opacity: '50%' } : { opacity: '100%' } }>
             <SingleCheckBtn
-              price={prices.Q72_Contact_form}
-              name={"Q72 Contact form"}
-              label={"Contact form +£170"}
-              storePrices={storePrices}
-              setStorePrices={setStorePrices}
+              price={ prices.Q72_Contact_form }
+              name={ "Q72 Contact form" }
+              label={ "Contact form +£170" }
+              storePrices={ storePrices }
+              setStorePrices={ setStorePrices }
             />
           </li>
-          <li>
+          <li style={ storePrices['Social media feed +£170'] ? { opacity: '50%' } : { opacity: '100%' } }>
             <SingleCheckBtn
-              price={prices.Q72_Social_media_feed}
-              name={"Q72 Social media feed"}
-              label={"Social media feed +£170"}
-              storePrices={storePrices}
-              setStorePrices={setStorePrices}
+              price={ prices.Q72_Social_media_feed }
+              name={ "Q72 Social media feed" }
+              label={ "Social media feed +£170" }
+              storePrices={ storePrices }
+              setStorePrices={ setStorePrices }
             />
           </li>
-          <li>
+          <li style={ storePrices['Q72 Live support chat'] ? { opacity: '50%' } : { opacity: '100%' } }>
             <SingleCheckBtn
-              price={prices.Q72_Live_support_chat}
-              name={"Q72 Live support chat"}
-              label={"Live support chat +£170"}
-              storePrices={storePrices}
-              setStorePrices={setStorePrices}
+              price={ prices.Q72_Live_support_chat }
+              name={ "Q72 Live support chat" }
+              label={ "Live support chat +£170" }
+              storePrices={ storePrices }
+              setStorePrices={ setStorePrices }
             />
           </li>
-          <li>
+          <li style={ storePrices['Q72 Event calendar'] ? { opacity: '50%' } : { opacity: '100%' } }>
             <SingleCheckBtn
-              price={prices.Q72_Event_calendar}
-              name={"Q72 Event calendar"}
-              label={"Event calendar +£510"}
-              storePrices={storePrices}
-              setStorePrices={setStorePrices}
+              price={ prices.Q72_Event_calendar }
+              name={ "Q72 Event calendar" }
+              label={ "Event calendar +£510" }
+              storePrices={ storePrices }
+              setStorePrices={ setStorePrices }
             />
           </li>
-          <li>
+          <li style={ storePrices['Q72 Branded Google Map'] ? { opacity: '50%' } : { opacity: '100%' } }>
             <SingleCheckBtn
-              price={prices.Q72_Branded_Google_Map}
-              name={"Q72 Branded Google Map"}
-              label={"Branded Google Map +£170"}
-              storePrices={storePrices}
-              setStorePrices={setStorePrices}
+              price={ prices.Q72_Branded_Google_Map }
+              name={ "Q72 Branded Google Map" }
+              label={ "Branded Google Map +£170" }
+              storePrices={ storePrices }
+              setStorePrices={ setStorePrices }
             />
           </li>
         </ul>
       </div>
 
       <Link to="/contacts-and-details">
-        <Button onClick={forwardAnime}>
-          <FontAwesomeIcon className="btn-arrow" icon={faArrowAltCircleRight} />{" "}
+        <Button onClick={ forwardAnime }>
+          <FontAwesomeIcon className="btn-arrow" icon={ faArrowAltCircleRight } />{ " " }
           Next step
         </Button>
       </Link>
