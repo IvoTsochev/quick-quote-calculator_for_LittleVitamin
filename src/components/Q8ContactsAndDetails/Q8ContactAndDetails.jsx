2b907da7caee9674a61e9fab052d741c5a4c2191
Ivo Tsochev <ivo.tsochev@live.com>
2022-09-24 18:53:25 +0300

    update

--- src/components/Q8ContactsAndDetails/Q8ContactAndDetails.jsx
+++ src/components/Q8ContactsAndDetails/Q8ContactAndDetails.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect, Fragment } from "react";
 import emailjs from "emailjs-com";
 import { Redirect } from 'react-router-dom'
 // Styles
